a8aa9f9f1fe0b8c68c00e71bf46d049584082c4e
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2022-02-21 18:48:37 -0600

    Create A_Suffix_Array_1.cpp

---
+++ Suffix_Array/A_Suffix_Array_1.cpp
@@ -0,0 +1,17 @@
+// Now try to implement the algorithm that was dicussed in the lecture: 
+// https://codeforces.com/edu/course/2/lesson/2
+
+// Write a program that reads the string s and prints the suffix array p for it
+
+// Input:
+//  Input contains a single string s of length n (1 <= n <= 100 000). String consists of small english letters.
+
+// Output:
+//  Print n + 1 distinct integers, indices of first characters of suffixes of s, ordered in lexicographical order.
+
+// Examples:
+//  ababba     ->  6 5 0 2 4 1 3 
+//  aaaa       ->  4 3 2 1 0
+//  ppppplppp  ->  9 5 8 4 7 3 6 2 1 0
+//  nn         ->  2 1 0
+
