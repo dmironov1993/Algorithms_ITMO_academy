55a27035b3c775da19f73635b555b51efec213a1
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2022-02-21 19:14:04 -0600

    Update A_Suffix_Array_1.cpp

--- Suffix_Array/A_Suffix_Array_1.cpp
+++ Suffix_Array/A_Suffix_Array_1.cpp
@@ -15,3 +15,49 @@
 //  ppppplppp  ->  9 5 8 4 7 3 6 2 1 0
 //  nn         ->  2 1 0
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main() {
+  std::string s;
+  std::cin >> s;
+  s += "$";
+  int n = s.size();
+  std::vector<int> p(n), c(n);
+  {
+    std::vector<std::pair<char,int>> a(n);
+    for (int i = 0; i < n; i++) a[i] = {s[i], i};
+    std::sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++) p[i] = a[i].second;
+    c[p[0]] = 0;
+    for (int i = 1; i < n; i++) 
+    {
+      if (a[i].first == a[i-1].first)
+        c[p[i]] = c[p[i-1]];
+      else
+        c[p[i]] = c[p[i-1]] + 1;
+    }
+  }
+  int k = 0;
+  while ((1 << k) < n)
+  {
+    std::vector<std::pair<std::pair<int, int>, int>> a(n);
+    for (int i = 0; i < n; i++) a[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
+    std::sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++) p[i] = a[i].second;
+    c[p[0]] = 0;
+    for (int i = 1; i < n; i++)
+    {
+      if (a[i].first == a[i-1].first)
+        c[p[i]] = c[p[i-1]];
+      else
+        c[p[i]] = c[p[i-1]] + 1;
+    }
+    k++;
+  }
+  for (int i = 0; i < n; i++)
+    std::cout << p[i] << " ";
+  return 0;
+}
